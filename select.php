6e494d13c30d17d93cf341478ce261b71e1afa42
6304062630202 <94558432+6304062630202@users.noreply.github.com>
2022-10-11 15:43:29 +0700

    Add files via upload

---
+++ select.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+<?php
+    if (empty($_COOKIE["lang"])){
+        setcookie("lang", $_GET["language"], time() + 10);
+    }
+
+    else{
+        $lang = $_GET["lang"];
+        setcookie("lang", $lang, time() + 10);
+    }
+
+    include 'main.php';
+
+?>
+</body>
+</html>
